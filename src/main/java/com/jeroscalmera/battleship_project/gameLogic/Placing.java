577805398a2dbd022f0337f4cd3c87058ddf1bc1
Jack Chaplin <136751073+JerosCalmera@users.noreply.github.com>
2024-05-28 19:02:05 +0100

    Update Placing.java

--- src/main/java/com/jeroscalmera/battleship_project/gameLogic/Placing.java
+++ src/main/java/com/jeroscalmera/battleship_project/gameLogic/Placing.java
@@ -330,21 +330,31 @@ public class Placing {
 
     int counter = 0;
 
-    // Logic so that the computer can automatically place ships in a random fashion
+    // Logic so that the computer can automatically place ships in a random fashion, if the placement was interrupted before, it will attempt to wait before overriding after a set time
     public void computerPlaceShips(String playerName) throws InterruptedException {
         System.out.println("Starting computer ship placement");
         Player player = playerRepository.findByNameContaining(playerName.substring(0,5));
+        List<Ship> ships = new ArrayList<>();
+        ships = shipRepository.findAllShipsByPlayerId(player.getId());
+        if (ships.size() > 1) {
+            System.out.println("Aborting placement");
+            return;
+        }
         if (shipPlacement) {
             if (!player.getName().contains("Computer")){
-            webSocketMessageSender.sendMessage("/topic/chat", new Chat(ChatToken.generateChatToken() + player.getRoomNumber() + "Admin: The computer is placing ships for another player, the next players placement will be attempted in 5 seconds"));}
+            webSocketMessageSender.sendMessage("/topic/chat", new Chat(ChatToken.generateChatToken() + player.getRoomNumber() + "Admin: The computer is placing ships for another player, the next players placement will be attempted in 5 seconds"));
+            System.out.println("Player delay");}
+            else {System.out.println("Computer delay");};
             if (counter >= 2) {
                 shipPlacement = false;
                 computerPlaceShips(playerName);
+                System.out.println("overriding");
                 return;
             }
             counter = counter + 1;
             Thread.sleep(5000);
             computerPlaceShips(playerName);
+            System.out.println("Counter state = " + counter);
             return;
         }
         shipPlacement = true;
